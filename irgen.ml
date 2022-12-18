6e8ee599fc75d7176894d92b2b62772539bed4a5
ellenguo6 <ellenguo6@gmail.com>
2022-12-18 01:12:53 -0500

    fixing function builder locations

--- irgen.ml
+++ irgen.ml
@@ -42,8 +42,8 @@ let translate program =
         L.function_type (ltype_of_typ ret) formal_types
   in
   (* Create stub entry point function "main" *)
-  let ftype = L.function_type i1_t (Array.of_list []) in
-  let builder =
+  let ftype = L.function_type i32_t (Array.of_list []) in
+  let builder_init =
     L.builder_at_end context
       (L.entry_block (L.define_function "main" ftype the_module))
   in
@@ -54,7 +54,8 @@ let translate program =
      a tuple of (value, new_builder) but i am not going to write this in just
      yet bc i am not 100% positive this is true, something something about
      how the builder updates itself?? -- alice this is your problem :P *)
-  let rec build_expr ((t, e) : shrexpr) (var_table : var_binding_map) =
+  let rec build_expr ((t, e) : shrexpr) (var_table : var_binding_map) builder
+      =
     match e with
     | SIntLit i -> L.const_int i32_t i
     | SBoolLit b -> L.const_int i1_t (if b then 1 else 0)
@@ -62,8 +63,8 @@ let translate program =
     | SCharLit c -> L.const_int i8_t (Char.code c)
     | SStringLit s -> L.build_global_stringptr s "tmp" builder
     | SInfixOp (e1, op, e2) ->
-        let e1' = build_expr e1 var_table
-        and e2' = build_expr e2 var_table in
+        let e1' = build_expr e1 var_table builder
+        and e2' = build_expr e2 var_table builder in
         (* t1 == t2 bc we semanted *)
         ( match op with
         | A.Add -> (
@@ -116,7 +117,7 @@ let translate program =
         | Not | UMinus | Cat | Cons | Head | Tail -> L.build_add )
           e1' e2' "tmp" builder
     | SUnaryOp (op, e1) ->
-        let e1' = build_expr e1 var_table in
+        let e1' = build_expr e1 var_table builder in
         ( match op with
         | UMinus -> (
           match e1 with
@@ -125,9 +126,9 @@ let translate program =
         | Not -> L.build_not )
           e1' "tmp" builder
     | SCondExp (condition, e1, e2) ->
-        let cond = build_expr condition var_table
-        and e1' = build_expr e1 var_table
-        and e2' = build_expr e2 var_table in
+        let cond = build_expr condition var_table builder
+        and e1' = build_expr e1 var_table builder
+        and e2' = build_expr e2 var_table builder in
         L.build_select cond e1' e2' "tmp" builder
     | SFunExp (formals, e) -> (
         let add_formals builder m f p =
@@ -155,29 +156,36 @@ let translate program =
               L.function_type (ltype_of_typ ret_type) lformal_types
             in
             let f = L.define_function "fexp" ftype the_module in
-            let body_bb = L.append_block context "body" f in
             let params_list = Array.to_list (L.params f) in
+            let entry_bb = L.entry_block f in
             let new_var_table =
               List.fold_left2
-                (add_formals (L.builder_at_end context body_bb))
+                (add_formals (L.builder_at_end context entry_bb))
                 var_table formals params_list
             in
             ignore
               (L.build_ret
-                 (build_expr e new_var_table)
-                 (L.builder_at_end context body_bb) ) ;
+                 (build_expr e new_var_table
+                    (L.builder_at_end context entry_bb) )
+                 (L.builder_at_end context entry_bb) ) ;
             f )
     | SFunApp (fexp, args) ->
         (* let f = match fexp with | _, SFunExp (_, _) -> build_expr fexp
            var_table | _, SVar id -> StringMap.find id var_table in *)
-        let f = build_expr fexp var_table in
-        let llargs = List.map (fun x -> build_expr x var_table) args in
+        let f = build_expr fexp var_table builder in
+        let llargs =
+          List.map (fun x -> build_expr x var_table builder) args
+        in
         L.build_call f (Array.of_list llargs) "result" builder
     | SAssign (id, rhs, exp) ->
         let new_var_table =
           match rhs with
           | (Function (_, _) as f), _ ->
-              let rhs' = build_expr rhs var_table in
+              (* let var = L.build_alloca (ltype_of_typ t) id builder in *)
+              (* let llt = L.function_type i1_t (Array.of_list [i1_t; i32_t])
+                 in print_endline ("hewwo " ^ L.string_of_lltype llt) ; *)
+              (* ignore (L.build_alloca llt "yaur" builder) ; *)
+              let rhs' = build_expr rhs var_table builder in
               StringMap.add id (f, rhs') var_table
               (* ALT *)
               (* let var = L.build_alloca (ltype_of_typ f) id builder in let
@@ -185,16 +193,20 @@ let translate program =
                  rhs' var builder) ; StringMap.add id (t, var) var_table *)
           | (_ as t), _ ->
               let var = L.build_alloca (ltype_of_typ t) id builder in
-              let rhs' = build_expr rhs var_table in
+              let rhs' = build_expr rhs var_table builder in
               ignore (L.build_store rhs' var builder) ;
               StringMap.add id (t, var) var_table
         in
-        build_expr exp new_var_table
+        build_expr exp new_var_table builder
     | SVar var -> (
         let v = StringMap.find var var_table in
         match v with
         | Function (_, _), llv -> llv
         | _, llv -> L.build_load llv var builder )
   in
-  ignore (L.build_ret (build_expr program StringMap.empty) builder) ;
+  ignore
+    (L.build_ret
+       (build_expr program StringMap.empty builder_init)
+       builder_init ) ;
+  ignore (L.build_ret (L.const_int i32_t 0) builder_init) ;
   the_module
