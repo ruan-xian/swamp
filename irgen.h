782ef62f935c2097882c939b3a3761d477981b45
kmiumiu <km3635@columbia.edu>
2022-12-19 21:07:21 -0500

    header files for c backend

--- irgen.h
+++ irgen.h
@@ -18,7 +18,11 @@ struct List {
 int shreksays(const char *fmt);
 
 struct List *newEmptyList();
-struct Node *newNode();
-struct List *appendNode();
+struct Node *newNode(void *val);
+struct List *appendNode(struct List *l, struct Node *n);
+struct List *catList(struct List *li, struct List *l2);
+struct List *consList(void *val, struct List *l); 
+void *getHead(struct List *l); 
+struct List *getTail(struct List *l); 
 
 #endif
